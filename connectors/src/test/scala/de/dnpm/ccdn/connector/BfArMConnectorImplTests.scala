b9be31e5de6a7299cf22897a9b58be9e8b28ac3e
Jonas Bollgrün <jonas.bollgruen@uni-tuebingen.de>
2026-02-10 14:53:55 +0100

    Added dependency for mocking framework. Managed to load test data and instantiate mock, but no mocked functionality yet

--- connectors/src/test/scala/de/dnpm/ccdn/connector/BfArMConnectorImplTests.scala
+++ connectors/src/test/scala/de/dnpm/ccdn/connector/BfArMConnectorImplTests.scala
@@ -1,10 +1,77 @@
 package de.dnpm.ccdn.connector
 
 import de.dnpm.ccdn.core.bfarm.SubmissionReport
-import org.scalatest.flatspec.{AnyFlatSpec, AsyncFlatSpec}
+import de.dnpm.ccdn.core.{Config, bfarm}
+import de.dnpm.dip.coding.Coding
+import de.dnpm.dip.model._
+import de.dnpm.dip.service.mvh.{Submission, TransferTAN, UseCase}
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.TestSuite
+import org.scalatest.flatspec.AsyncFlatSpec
+import play.api.libs.ws.{StandaloneWSClient, StandaloneWSRequest}
 
-import scala.concurrent.{ExecutionContext, Future}
+import java.time.LocalDateTime
+import java.util.UUID.randomUUID
 
-final class BfArMConnectorImplTests extends AsyncFlatSpec{
+class BfArMConnectorImplTests extends AsyncFlatSpec with TestSuite with MockFactory /* Stubs? */ {
+  //uses custom config.json
+
+  private def rndReport: Submission.Report =
+    Submission.Report(
+      Id[TransferTAN](randomUUID.toString),
+      LocalDateTime.now,
+      Id[Patient](randomUUID.toString),
+      Submission.Report.Status.Unsubmitted,
+      Coding[Site]("UKFR"),
+      UseCase.MTB,
+      Submission.Type.Initial,
+      Some(NGSReport.Type.GenomeLongRead),
+      None,
+      HealthInsurance.Type.UNK,
+      None,
+      None
+    )
+  private val BfarmReport: Submission.Report => bfarm.SubmissionReport = {
+
+    import NGSReport.Type._
+    import bfarm.LibraryType
+    import bfarm.SubmissionReport.DiseaseType._
+    import de.dnpm.dip.service.mvh.UseCase._
+
+    report =>
+      bfarm.SubmissionReport(
+        report.createdAt.toLocalDate,
+        report.`type`,
+        report.id,
+        Config.instance.submitterId(report.site.code),
+        Config.instance.dataNodeIds(report.useCase),
+        report.useCase match {
+          case MTB => Oncological
+          case RD  => Rare
+        },
+        report.sequencingType.collect {
+            case GenomeLongRead  => LibraryType.WGSLr
+            case GenomeShortRead => LibraryType.WGS
+            case Exome           => LibraryType.WES
+            case Panel           => LibraryType.Panel
+          }
+          .getOrElse(LibraryType.None),
+        report.healthInsuranceType
+      )
+  }
+
+  val testSubmissions:List[SubmissionReport] = List.fill(10) (BfarmReport(rndReport))
+  val mockHttpClient = stub[StandaloneWSClient]
+  val stubRequest = stub[StandaloneWSRequest]
+  (mockHttpClient.url _).expects("asd").returns(stubRequest)
+
+  val toTest = new BfArMConnectorImpl(
+    BfArMConnectorImpl.Config.instance,
+    mockHttpClient)
+  for (curSubm <- testSubmissions) {
+    toTest.upload(curSubm)
+  }
+
+
 
 }
--- core/src/test/scala/de/dnpm/ccdn/core/MVHReportingServiceTests.scala
+++ core/src/test/scala/de/dnpm/ccdn/core/MVHReportingServiceTests.scala
@@ -11,8 +11,11 @@ final class MVHReportingServiceTests extends AsyncFlatSpec
 {
 
   val reportStore    = FakeReportRepository
+  //becomes instance of FakeDNPMConnector, through custom Service Provider config
   val dipConnector   = dip.Connector.getInstance.get
+  //becomes instance of FakeBfArMConnector
   val bfarmConnector = bfarm.Connector.getInstance.get
+  //actually setting to the actual object works just as well
 
   val service =
     new MVHReportingService(
