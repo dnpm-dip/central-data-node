e7f6a081f868551259c13bd85d459628897af69f
Jonas Bollgrün <jonas.bollgruen@uni-tuebingen.de>
2026-02-20 14:31:29 +0100

    Added unit test against stuck invalid tokenfutures, which right now fails (and should)

--- connectors/src/test/scala/de/dnpm/ccdn/connector/BfArMConnectorImplTests.scala
+++ connectors/src/test/scala/de/dnpm/ccdn/connector/BfArMConnectorImplTests.scala
@@ -40,9 +40,9 @@ class BfArMConnectorImplTests extends AsyncFlatSpec
 
   //how many documents are uploaded
   val nUploads = 10
-  val LongTokenLifetime = 133742
+  val LongTokenLifetime = 133742 //random amount of seconds long enough to run the test
   val ZeroTokenLifetime = 5 //BfArMConnectorImpl clears the token 5 seconds before formal expiration
-  val apiUrlAddress ="somewhere"
+  val apiUrlAddress ="somewhere" //just random values
   val authUrlAddress ="someplaceelse"
   //increments every time a token is fetched, should increase once
   val bfarmConnectorConfig = BfArMConnectorImpl.Config(
@@ -55,6 +55,7 @@ class BfArMConnectorImplTests extends AsyncFlatSpec
   }
   trait CustomResponse extends StandaloneWSResponse { }
 
+  class CustomTestException extends Exception("arsrtfhg")
 
   implicit val format: Writes[Token] =
     Json.writes[Token]
@@ -68,7 +69,7 @@ class BfArMConnectorImplTests extends AsyncFlatSpec
 
 
 
-  private class TestFixture(tokenExpiration:Int) {
+  private class TestFixture(tokenExpiration:Int,tokenFetchSucceeds:Boolean) {
     val tokenFetchCounter = new AtomicInteger(0)
     val mockHttpClient:StandaloneWSClient = stub[StandaloneWSClient]
 
@@ -82,7 +83,10 @@ class BfArMConnectorImplTests extends AsyncFlatSpec
       .when(*,*)
       .onCall(_ => {
         tokenFetchCounter.incrementAndGet()
-        Future.successful(mockAuthResponse)
+        if(tokenFetchSucceeds)
+          Future.successful(mockAuthResponse)
+        else
+          Future.failed(new CustomTestException())
       })
     (() => mockAuthResponse.status).when().returns(200)
     //point of this test: token should only be requested once
@@ -108,7 +112,7 @@ class BfArMConnectorImplTests extends AsyncFlatSpec
   ////////////////////////////////////////////////// Testcases
 
   it must "only fetch one token to make multiple uploads in short time" in {
-    val fixture = new TestFixture(LongTokenLifetime)
+    val fixture = new TestFixture(LongTokenLifetime,true)
 
     assert(fixture.tokenFetchCounter.get() == 0)
     val submissionReports = List.fill(nUploads)(makeFakeReport)
@@ -120,17 +124,48 @@ class BfArMConnectorImplTests extends AsyncFlatSpec
 
 
   it must "fetch a new token for subsequent uploads if it expires during a series of uploads" in {
-    val fixture = new TestFixture(ZeroTokenLifetime)
+    val fixture = new TestFixture(ZeroTokenLifetime,true)
 
     for{
-      _ <- assert(fixture.tokenFetchCounter.get() == 0)
+      _ <- (assertResult(0, "Tokenfetchcounter should be initialized to zero before test")
+              (fixture.tokenFetchCounter.get()))
       _ <- fixture.toTest.upload(makeFakeReport)
-      _ <- assert(fixture.tokenFetchCounter.get() == 1)
+      _ <- assertResult(1, "Tokenfetch appearantly did not tirgger")(fixture.tokenFetchCounter.get())
       _ <- fixture.toTest.upload(makeFakeReport)
-      _ <- assert(fixture.tokenFetchCounter.get() == 2)
+      _ <- assertResult(2)(fixture.tokenFetchCounter.get()) //no clue why it failed
       _ <- fixture.toTest.upload(makeFakeReport)
-      _ <- assert(fixture.tokenFetchCounter.get() == 3)
+      _ <- assertResult(3)(fixture.tokenFetchCounter.get())
     } yield succeed
+  }
+
+
+  it must "fetch a new token for subsequent uploads if the first fetch failed" in {
+    val fixture = new TestFixture(LongTokenLifetime,false)
+
+    //TODO fails right now, probably should, but not sure
+
+    for{
+      _                   <- (assertResult(0, "Tokenfetchcounter should be initialized to zero before test")
+                                          (fixture.tokenFetchCounter.get()))
+      firstUploadAttempt  <- fixture.toTest.upload(makeFakeReport).recover[Either[String,Unit]](
+                                {case _:CustomTestException => Left("testFehler1")})
+      _                   <- assertResult(1, "Tokenfetch appearantly did not trigger")(fixture.tokenFetchCounter.get())
+      secondUploadAttempt <- fixture.toTest.upload(makeFakeReport).recover[Either[String,Unit]](
+                                {case _:CustomTestException => Left("testFehler2")})
+      _                   <- assertResult(2)(fixture.tokenFetchCounter.get())
+    } yield {
+      assert(firstUploadAttempt.isLeft)
+      assert(secondUploadAttempt.isLeft)
+    }
+
+  }
+
+
+  it must "fail the upload when the fetch of the token fails" in {
+    val fixture = new TestFixture(LongTokenLifetime,false)
+    recoverToSucceededIf[CustomTestException] {
+      fixture.toTest.upload(makeFakeReport)
+    }
   }
 
 
@@ -144,13 +179,14 @@ class BfArMConnectorImplTests extends AsyncFlatSpec
       9876,
       "khzhftgre",
       "bcvgfhjuz")
-    val backAndForth = Json.fromJson[Token](Json.toJson[Token](testToken))
+    val serializedToken = Json.toJson[Token](testToken)
+    val deserializedToken = Json.fromJson[Token](serializedToken)
 
-    assert(backAndForth.isSuccess)
-    assert(testToken.access_token == backAndForth.get.access_token)
-    assert(testToken.token_type == backAndForth.get.token_type)
-    assert(testToken.scope == backAndForth.get.scope)
-    assert(testToken.expires_in == backAndForth.get.expires_in)
-    assert(testToken.refresh_expires_in == backAndForth.get.refresh_expires_in)
+    assert(deserializedToken.isSuccess,"Serialization (or deserialization) failed")
+    assertResult(testToken.access_token)(deserializedToken.get.access_token)
+    assertResult(testToken.token_type)(deserializedToken.get.token_type)
+    assertResult(testToken.scope)(deserializedToken.get.scope)
+    assertResult(testToken.expires_in)(deserializedToken.get.expires_in)
+    assertResult(testToken.refresh_expires_in)(deserializedToken.get.refresh_expires_in)
   }
 }
