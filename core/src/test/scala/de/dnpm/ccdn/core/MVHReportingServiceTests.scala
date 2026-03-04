eafad005a57477db83e60727d9795ddbda48a4a3
Jonas Bollgrün <jonas.bollgruen@uni-tuebingen.de>
2026-03-04 15:56:18 +0100

    Unit Test does what it should, now cleanup

--- core/src/test/scala/de/dnpm/ccdn/core/MVHReportingServiceTests.scala
+++ core/src/test/scala/de/dnpm/ccdn/core/MVHReportingServiceTests.scala
@@ -52,25 +52,27 @@ final class MVHReportingServiceTests extends AsyncFlatSpec
     result
   }
 
-  it must "not process more submissions simultaneously than it has threads" in {
+  it must "(non-deterministic) not process more submissions simultaneously than it has threads" in {
     //configure bfarmconnecteor to halt for 100 msec during upload
     //testing setup has 39 clinic-usecases, so there can be no less than 39 uploads to run
-    val fixture = new TestFixture(1,true) //TODO tatsächlich gibts 40*39 uploads, weil es 39 Klinikusecases in der config.json
+    val fixture = new TestFixture(1,true)
 
     //have more reports overall than nThreads
     val expectedNumReports = Config.instance.sites.flatMap(it => it._2.useCases).size //39
     assert(expectedNumReports > MVHReportingService.nThreads)
-    //but no more than twice as many
+    //but no more than twice as many, so that a boolean predicate works to split one set of timings in two
     assert(expectedNumReports <= MVHReportingService.nThreads*2)
 
     //run
-    val startTime = System.currentTimeMillis()
     Await.result(fixture.service.pollReports,Duration(5,SECONDS))
     Await.result(fixture.service.uploadReports,Duration(5,SECONDS))
 
+
     val allUploadTimings = fixture.bfarmConnector.uploadFinishTimings.get()
-    //expect that theres a contingent of uploads that all happened within 50msec, 100msec after the simulation started
-    def firstUploadPredicate:Long=>Boolean = _ - startTime - FakeBfArMConnector.uploadDelayMsec < 50
+    //expect that theres a contingent of uploads that all happened within 25msec of the first
+    val minTiming = allUploadTimings.min
+    val maxTimeDifference = 25 //generous
+    def firstUploadPredicate:Long=>Boolean = _ - minTiming < maxTimeDifference
     val firstUploads = allUploadTimings.filter(firstUploadPredicate)
     val remainingUploads = allUploadTimings.filter(!firstUploadPredicate(_))
 
@@ -78,11 +80,12 @@ final class MVHReportingServiceTests extends AsyncFlatSpec
     assertResult(expectedNumReports)(allUploadTimings.size)
     assertResult(expectedNumReports)(firstUploads.size+remainingUploads.size)
 
-    //verify that <nThreads> submissions were transmitted at pretty much the same time (within 30 msec) and that m were at least 100 msec later
+    //verify that the first contingent has exactly <nThreads> items
     assertResult(MVHReportingService.nThreads)(firstUploads.size)
+    //verify that the rest is equally close
+    val minRemainderTiming = remainingUploads.min
+    assert(remainingUploads.forall(_ - minRemainderTiming < maxTimeDifference))
 
-
-    //TODO implement
     fixture.service.stop()
     succeed
   }
