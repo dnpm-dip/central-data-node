9e63cfdd90284b1295dafd84ffbe738239fdd2cc
Lucien Clin <lucien.clin@uni-tuebingen.de>
2026-04-24 09:14:44 +0200

    refactor: Generalized BatchingUtil.batchTraverse to work with any Iterable[_] instead of just Seq[_]

--- core/src/main/scala/de/dnpm/ccdn/core/BatchingUtil.scala
+++ core/src/main/scala/de/dnpm/ccdn/core/BatchingUtil.scala
@@ -1,6 +1,7 @@
 package de.dnpm.ccdn.core
 
 
+import scala.collection.IterableOps
 import cats.syntax.applicative._
 import cats.syntax.flatMap._
 import cats.syntax.functor._
@@ -9,32 +10,33 @@ import cats.syntax.traverse._
 
 trait BatchingUtil
 {
+
   /**
-   * Traverse a Seq[T] with an effectful transformation T => F[U] 
+   * Traverse a Collection C[_] with an effectful transformation T => F[U] 
    * in successive (i.e. monadically sequenced) batches.
    * Monadic chaining ensures the batches are always processed sequentially, 
    * even in case of a "false" effect like scala.concurrent.Future[T],
    * which is evaluated eagerly.
    *
-   * @param ts Sequence of T elements
+   * @param ts Collection of T elements
    * @param batchSize Batch size into which to split [[ts]]
    * @param f Effectful tranformation: T => F[U] 
-   * @return Sequence Seq[U] of transformation results wrapped in F[_] 
+   * @return Collection C[U] of transformation results wrapped in F[_] 
    */
-  def batchTraverse[T,F[_]: cats.Monad,U](
-    ts: Seq[T],
+  def batchTraverse[T,C[x] <: IterableOps[x,C,C[x]],F[_]: cats.Monad,U](
+    ts: C[T],
     batchSize: Int
   )(
     f: T => F[U]
-  ): F[Seq[U]] =
+  ): F[C[U]] =
     ts.grouped(batchSize)
       .foldLeft(
-        Seq.empty[U].pure // Load empty accumulator into Monad
+        ts.iterableFactory.empty[U].pure // Load empty accumulator into Monad
       )(
         (acc,batch) =>
           for { 
             results <- acc
-            batchResults <- batch traverse f
+            batchResults <- batch.toList traverse f
           } yield results ++ batchResults
       )
 
